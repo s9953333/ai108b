876d7ac0e1d584279c60a47aabf09ae748042329
s9953333 <62669836+s9953333@users.noreply.github.com>
2020-06-25 17:37:31 +0800

    Update AND.py

--- 期末報告/02-爬山演算法/AND.py
+++ 期末報告/02-爬山演算法/AND.py
@@ -1,54 +1,24 @@
+"""
+A X = B ，求 X 是多少？
+範例：題目來源: http://mail.im.tku.edu.tw/~idliaw/LinTup/99ie/99IEntu.pdf
+4a+3b+6c=1
+1a+1b+2c=2
+2a+1b+3c=-1
+"""
+import gd1 as gd
 import numpy as np
-from numpy.linalg import norm
 
-# 函數 f 對變數 k 的偏微分: df / dk
-def df(f, p, k, step=0.01):
-    p1 = p.copy()
-    p1[k] = p[k]+step
-    return (f(p1) - f(p)) / step
-
-# 函數 f 在點 p 上的梯度
-def grad(f, p, step=0.01):
-    gp = p.copy()
-    for k in range(len(p)):
-        gp[k] = df(f, p, k, step)
-    return gp
-
-# 使用梯度下降法尋找函數最低點
-def gradientDescendent(f, p0, step=0.01):
-    p = p0.copy()
-    i = 0
-    fp0 = f(p)
-    while (True):
-        i += 1
-        fp = f(p)
-        gp = grad(f, p) # 計算梯度 gp
-        glen = norm(gp) # norm = 梯度的長度 (步伐大小)
-        print('{:d}:p={:s} f(p)={:.3f} gp={:s} glen={:.5f}'.format(i, str(p), fp, str(gp), glen))
-        if glen < 0.00001 or fp0 < fp:  # 如果步伐已經很小了，或者 f(p) 變大了，那麼就停止吧！
-            break
-        gstep = np.multiply(gp, -1*step) # gstep = 逆梯度方向的一小步
-        p +=  gstep # 向 gstep 方向走一小步
-        fp0 = fp
-    return p # 傳回最低點！
-
-A = np.array([[0.0,0,1],
-                [1,0,1],
-                [0,1,1],
-                [1,1,1]])
-
-B = np.array([0.0,0,0,1]).transpose()
+# A = np.array([[4.0,3,6],[1,1,2],[2,1,3]])
+# B = np.array([[1.0,2,-1]]).transpose()
+A = np.array([[3.0,2],[1,1]])
+B = np.array([[5.0, 2]]).transpose()
 
 def f(p): #  能量函數:計算 ||AX-B||，也就是 ||Y-B||
     X = p
     Y = A.dot(X)
+    # return np.linalg.norm(Y-B, 2)
     return np.linalg.norm(Y-B, 1)
 
-p = np.array([1.0,1,1])
-p=gradientDescendent(f, p, step=0.01)
-
-print("\n==================================================================================\n")
-
-print("p = {}".format(p))
-
-print('w1={} w2={} b={} '.format(p[0], p[1], p[2]))
+# p = np.array([2.0, 2.0, 2.0])
+p = np.array([0.0,0])
+gd.gradientDescendent(f, p, step=0.01)
